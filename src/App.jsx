8ad23c0796b09ea41070acf3de88d864bb777ec5
yigitaksoy <yigitaksoy@gmail.com>
2023-11-01 12:55:38 +0100

    Initial commit :octocat:

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./assets/css/style.css";
+
+function App() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-house-black font-objektiv">
+      <p className="text-house-white text-8xl text-center font-heavy">
+        Hey! 👋
+      </p>
+    </div>
+  );
+}
+
+export default App;
