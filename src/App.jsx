4682bae5104503979be190e97c5e95fe0af8b43e
yigitaksoy <yigitaksoy@gmail.com>
2023-11-05 20:33:39 +0100

    Adjust app and hero section background and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import { AnimatePresence } from "framer-motion";
 import "./assets/css/style.css";
 import Hero from "./sections/Hero";
+import { GridPattern } from "./components/GridPattern";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="bg-house-black font-objektiv">
-      <Hero />
+    <div className="font-objektiv">
+      <BrowserRouter>
+        <AnimatePresence></AnimatePresence>
+        <AnimatePresence mode="wait">
+          <main className="relative isolate flex w-full flex-col bg-white mt-2 pt-14 overflow-hidden">
+            <GridPattern
+              className="absolute inset-x-0 -top-14 -z-10 h-[1000px] w-full fill-neutral-50 stroke-neutral-950/5 [mask-image:linear-gradient(to_bottom_left,white_40%,transparent_50%)]"
+              yOffset={-96}
+              interactive
+            />
+            <Hero />
+          </main>
+        </AnimatePresence>
+      </BrowserRouter>
     </div>
   );
 }
