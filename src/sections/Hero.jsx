9cf04b653ac5d711f7fc2ae60a5877ccb15beccf
yigitaksoy <yigitaksoy@gmail.com>
2023-11-05 17:00:32 +0100

    Add grid pattern background to hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,16 @@
+import { GridPattern } from "../components/GridPattern";
+
 const Hero = () => {
   return (
-    <section className="flex h-screen w-screen items-center justify-center ">
-      <p className="text-house-white text-8xl text-center font-heavy">
-        Hey! 👋
-      </p>
+    <section
+      id="hero"
+      className="flex h-screen w-screen items-center justify-center "
+    >
+      <GridPattern
+        className="absolute inset-x-0 -top-14 -z-10 h-screen w-full fill-neutral-100 stroke-neutral-950/5 [mask-image:linear-gradient(to_bottom_left,white_40%,transparent_50%)]"
+        yOffset={-96}
+        interactive
+      />
     </section>
   );
 };
