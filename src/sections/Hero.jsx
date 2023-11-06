8b63ea62f8fa70db4a2526e5e8d235b2f3a02a79
yigitaksoy <yigitaksoy@gmail.com>
2023-11-06 01:11:15 +0100

    Update heading and subtitle text

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,14 +5,13 @@ const Hero = () => {
   return (
     <section id="hero" className="h-screen">
       <Container className="mt-36 sm:mt-32 md:mt-60">
-        <FadeIn className="max-w-3xl">
+        <FadeIn className="max-w-2xl">
           <h1 className="font-display text-4xl font-heavy tracking-tight text-white [text-wrap:balance] sm:text-6xl">
-            Digital Studio based in the heart of Amsterdam.
+            Crafting Digital Experiences in the heart of Amsterdam.
           </h1>
-          <p className="mt-6 text-xl text-house-100">
-            We are developer studio working at the intersection of design and
-            technology. It’s a really busy intersection though — a lot of our
-            staff have been involved in hit and runs.
+          <p className="mt-6 text-xl text-house-white">
+            We focus on clear design and dependable technology to develop
+            easy-to-use web and e-commerce platforms.
           </p>
         </FadeIn>
       </Container>
