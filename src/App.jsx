11af65be6a090c00af1b7530e0568620cdf4f18d
yigitaksoy <yigitaksoy@gmail.com>
2023-11-09 11:30:36 +0100

    Change gradient pattern stroke color

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
         <AnimatePresence mode="wait">
           <main className="relative isolate flex w-full flex-col bg-house-black mt-2 pt-14 overflow-hidden">
             <GridPattern
-              className="absolute inset-x-0 -top-14 -z-10 h-[1000px] w-full fill-house-bluelight stroke-house-600 [mask-image:linear-gradient(to_bottom_left,white_40%,transparent_50%)]"
+              className="absolute inset-x-0 -top-14 -z-10 h-[1000px] w-full fill-house-bluelight  stroke-neutral-900 [mask-image:linear-gradient(to_bottom_left,white_40%,transparent_50%)]"
               yOffset={-96}
               interactive
             />
