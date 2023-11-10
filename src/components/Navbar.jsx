584c0184ce05e3df67e8a89603605e72a5b6c7ca
yigitaksoy <yigitaksoy@gmail.com>
2023-11-10 11:45:09 +0100

    Enhance navbar UX with dynamic padding and auto-hide on scroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,20 +26,43 @@ const Navbar = () => {
 
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
+      const isTop = currentScrollY < 50;
+      const isScrollingDown = currentScrollY > prevScrollY.current;
+      const hideNavbar = currentScrollY > 500;
 
-      const isTop = window.scrollY < 50;
-
-      // Show or hide navbar based on scroll direction
-      gsap.to(navRef.current, {
-        yPercent: currentScrollY > prevScrollY.current && !isTop ? -100 : 0,
-        ease: "power1.out",
-        duration: 0.6,
-      });
-
-      // Update navbar background based on scroll position
+      // Always set the navbar background based on scroll position
       setNavBackground(
         currentScrollY > 50 ? "bg-house-black" : "bg-transparent",
       );
+
+      if (isTop) {
+        gsap.to(navRef.current, {
+          paddingTop: "3.5rem",
+          paddingBottom: "3.5rem",
+          autoAlpha: 1,
+          yPercent: 0,
+          ease: "power1.out",
+          duration: 0.6,
+        });
+      } else {
+        gsap.to(navRef.current, {
+          paddingTop: "1.75rem",
+          paddingBottom: "1.75rem",
+          autoAlpha: 1,
+          ease: "power1.out",
+          duration: 0.6,
+        });
+
+        // After a certain amount of scroll, hide or show the navbar based on the direction
+        if (hideNavbar) {
+          gsap.to(navRef.current, {
+            yPercent: isScrollingDown ? -100 : 0,
+            autoAlpha: isScrollingDown ? 0 : 1, // This line should be conditional
+            ease: "power1.out",
+            duration: 1,
+          });
+        }
+      }
 
       prevScrollY.current = currentScrollY;
     };
@@ -75,7 +98,7 @@ const Navbar = () => {
     <nav
       id="navbar"
       ref={navRef}
-      className={`fixed top-0 left-0 w-full z-50 flex justify-between items-center px-10 lg:px-16 py-7 transition-colors duration-300 ${navBackground}`}
+      className={`fixed top-0 left-0 w-full z-50 flex justify-between items-center px-10 lg:px-16 lg:py-14 py-7 transition-colors duration-300 ${navBackground}`}
       role="navigation"
     >
       <Link to="/" onClick={handleLogoClick}>
