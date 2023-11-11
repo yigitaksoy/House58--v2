ce1131e0d392473f189a85a9d51b32d351a23b98
yigitaksoy <yigitaksoy@gmail.com>
2023-11-11 15:51:54 +0100

    Remove AnimatePresence

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { AnimatePresence } from "framer-motion";
 import "./assets/css/style.css";
 import { GridPattern } from "./components/GridPattern";
 import { BrowserRouter } from "react-router-dom";
@@ -17,21 +16,20 @@ function App() {
           <Navbar />
         </header>
         <ScrollTop />
-        <AnimatePresence mode="wait">
-          <main className="relative isolate flex w-full flex-col bg-house-black mt-2 pt-14 overflow-hidden">
-            <GridPattern
-              className="absolute inset-x-0 -top-14 -z-10 h-[1000px] w-full fill-house-bluelight  stroke-neutral-900 [mask-image:linear-gradient(to_bottom_left,white_40%,transparent_50%)]"
-              yOffset={-96}
-              interactive
-            />
-            <Hero />
-            <Services />
-            <TechStack />
-          </main>
-          <footer>
-            <Footer />
-          </footer>
-        </AnimatePresence>
+
+        <main className="relative isolate flex w-full flex-col bg-house-black mt-2 pt-14 overflow-hidden">
+          <GridPattern
+            className="absolute inset-x-0 -top-14 -z-10 h-[1000px] w-full fill-house-bluelight  stroke-neutral-900 [mask-image:linear-gradient(to_bottom_left,white_40%,transparent_50%)]"
+            yOffset={-96}
+            interactive
+          />
+          <Hero />
+          <Services />
+          <TechStack />
+        </main>
+        <footer>
+          <Footer />
+        </footer>
       </BrowserRouter>
     </div>
   );
