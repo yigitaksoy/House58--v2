4ee4f923fbff782b0805eb8ba4c2acb66cff07a8
yigitaksoy <yigitaksoy@gmail.com>
2023-11-12 12:56:07 +0100

    Change mobile nav layout

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -87,13 +87,47 @@ const MobileNav = () => {
     <div ref={el} className="fixed top-0 left-0 w-full">
       <div className="nav-bg fixed top-0 left-0 bg-house-black w-full h-screen opacity-0 pointer-events-none"></div>
       <div className="fixed right-0 top-0 w-full md:w-[500px] h-screen pointer-events-none overflow-y-scroll">
-        <div className="nav-side bg-house-black w-full h-full translate-x-[140%] relative pointer-events-auto">
+        <div
+          id="overlay-nav-layout"
+          className="nav-side bg-house-black w-full h-full translate-x-[140%] relative pointer-events-auto"
+        >
           <div className="nav-round absolute left-0 -translate-x-1/2 h-[110vh] translate-y-[-5vh] rounded-[100%/100%] w-[80%] bg-house-black top-0"></div>
-          <div className="nav-links text-left text-white opacity-0">
-            <div className="text-[clamp(2.5rem,10vw,3.75rem)] absolute top-[45%] -translate-y-1/2 pl-8 lg:pl-10 w-full items-center">
+          <div className="nav-links pointer-events-auto fixed top-0 right-0 z-20 flex h-full w-full max-w-lg flex-col justify-between bg-house-black pt-[clamp(3.5rem,10vh,5rem)] pb-12 text-6xl text-white">
+            <div className="px-[clamp(1.25rem,3vw,2.5rem)]">
+              <span className="text-sm text-neutral-500">Navigation</span>
+              <div className="mt-4 mb-4 h-[1px] w-full bg-neutral-600"></div>
+            </div>
+            {/* Navigation Links */}
+            <div className="text-[clamp(3rem,10vw,3.75rem)] absolute top-[40%] -translate-y-1/2  w-full items-center">
               <Link
                 to="/"
-                className="group flex items-center py-[calc(2vh+5px)] cursor-pointer w-full"
+                className="group flex items-center justify-between p-3 cursor-pointer"
+                onClick={toggleNav}
+              >
+                <p
+                  className={`group-hover:translate-x-[20px] transition font-heavy ${
+                    open ? "" : "translate-x-40px] duration-300 "
+                  }`}
+                >
+                  About
+                </p>
+              </Link>
+              <Link
+                to="/services"
+                className="group flex items-center justify-between p-3 cursor-pointer"
+                onClick={toggleNav}
+              >
+                <p
+                  className={`group-hover:translate-x-[20px] transition font-heavy ${
+                    open ? "" : "translate-x-[50px] duration-300"
+                  }`}
+                >
+                  Services
+                </p>
+              </Link>
+              <Link
+                to="/contact"
+                className="group flex items-center justify-between p-3 cursor-pointer"
                 onClick={toggleNav}
               >
                 <p
@@ -101,70 +135,37 @@ const MobileNav = () => {
                     open ? "" : "translate-x-[60px] duration-300 "
                   }`}
                 >
-                  Services
-                </p>
-              </Link>
-              <Link
-                to="/"
-                className="group flex items-center py-[calc(2vh+5px)] cursor-pointer w-full"
-                onClick={toggleNav}
-              >
-                <p
-                  className={`group-hover:translate-x-[20px] transition font-heavy ${
-                    open ? "" : "translate-x-[60px] duration-300"
-                  }`}
-                >
-                  About
-                </p>
-              </Link>
-              <Link
-                to="/"
-                className="group flex items-center py-[calc(2vh+5px)] cursor-pointer w-full"
-                onClick={toggleNav}
-              >
-                <p
-                  className={`group-hover:translate-x-[20px] font-heavy transition ${
-                    open ? "" : "translate-x-[60px] duration-300"
-                  }`}
-                >
                   Contact
                 </p>
               </Link>
             </div>
-            <div className="absolute bottom-20 lg:bottom-12 px-[5vw] lg:px-8 flex">
-              <a
-                href="https://www.linkedin.com/company/house-58-digital/"
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="LinkedIn"
-              >
-                <div
-                  className={`group p-6 w-fit cursor-pointer transition ${
+            <div className="px-4 mt-[25vh]">
+              <span className="text-sm text-neutral-500">Links</span>
+              <div className="mt-4 mb-4 h-[1px] bg-neutral-600"></div>
+              <div className="flex text-sm">
+                <a
+                  href="https://www.linkedin.com/company/house-58-digital/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="LinkedIn"
+                  className={`group transition mr-5 ${
                     open ? "" : "translate-x-[40px] duration-300"
                   }`}
                 >
-                  <p className="transition group-hover:-translate-y-1 group-hover:text-white">
-                    LinkedIn
-                  </p>
-                  <div className="h-[2px] bg-white scale-x-0 group-hover:scale-x-100 transition duration-200 origin-center"></div>
-                </div>
-              </a>
-              <a
-                href="mailto:hello@house58.nl?subject=Lets%20Start%20a%20Project"
-                aria-label="Email"
-              >
-                <div
-                  className={`group p-6 w-fit cursor-pointer transition ${
-                    open ? "" : "translate-x-[80px] duration-300"
+                  <p>LinkedIn</p>
+                </a>
+                <a
+                  href="mailto:hello@house58.nl?subject=Lets%20Start%20a%20Project"
+                  aria-label="Email"
+                  className={`group transition ${
+                    open ? "" : "translate-x-[40px] duration-300"
                   }`}
                 >
-                  <p className="transition group-hover:-translate-y-1 group-hover:text-white">
-                    Email
-                  </p>
-                  <div className="h-[2px] bg-white scale-x-0 group-hover:scale-x-100 transition duration-200 origin-center"></div>
-                </div>
-              </a>
+                  <p>Email</p>
+                </a>
+              </div>
             </div>
+            <div className="px-4"></div>
           </div>
         </div>
       </div>
